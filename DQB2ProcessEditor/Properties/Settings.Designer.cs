3b2f2284ed35922573ae8a4d3f92ab7f4de8e384
turtle-insect <turtle.insect@gmail.com>
2022-06-16 00:11:08 +0900

    change item's count value

--- DQB2ProcessEditor/Properties/Settings.Designer.cs
+++ DQB2ProcessEditor/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DQB2ProcessEditor.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace DQB2ProcessEditor.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("500")]
+        [global::System.Configuration.DefaultSettingValueAttribute("777")]
         public ushort ItemCount {
             get {
                 return ((ushort)(this["ItemCount"]));
