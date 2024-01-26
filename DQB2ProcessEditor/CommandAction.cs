9a36c0596f03bec04b14e64d2fd8f636e334d866
turtle-insect <turtle.insect@gmail.com>
2024-01-27 05:22:32 +0900

    support color pattern

--- DQB2ProcessEditor/CommandAction.cs
+++ DQB2ProcessEditor/CommandAction.cs
@@ -6,7 +6,9 @@ namespace DQB2ProcessEditor
 	internal class CommandAction : ICommand
 	{
 		private readonly Action<object?> mAction;
+#pragma warning disable CS0067
 		public event EventHandler? CanExecuteChanged;
+#pragma warning restore CS0067
 		public bool CanExecute(object? parameter) => true;
 		public CommandAction(Action<object?> action) => mAction = action;
 		public void Execute(object? parameter) => mAction(parameter);
---
+++ DQB2ProcessEditor/ItemColorConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DQB2ProcessEditor
+{
+	internal class ItemColorConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var info = value as ItemInfo;
+			if (info == null) return "";
+
+			if (info.Name?.IndexOf("(白)") > 0) return "White";
+			if (info.Name?.IndexOf("(黒)") > 0) return "Black";
+			if (info.Name?.IndexOf("(紫)") > 0) return "Purple";
+			if (info.Name?.IndexOf("(ピンク)") > 0) return "Pink";
+			if (info.Name?.IndexOf("(赤)") > 0) return "Red";
+			if (info.Name?.IndexOf("(緑)") > 0) return "Green";
+			if (info.Name?.IndexOf("(黄)") > 0) return "Yellow";
+			if (info.Name?.IndexOf("(青)") > 0) return "Blue";
+			return "";
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
